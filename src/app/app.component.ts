6bb163ec99bc07eeec0552cbfb075f1a6f211152
Javier Cabero <javier.cabero@hotmail.com>
2017-12-06 23:06:46 +0100

    added dynamic load of 10 more entries each time the user reaches bottom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
 
   private current_post_data_url = 'http://xkcd.com/info.0.json';
-  private initial_gag_count = 10;
+  private load_more_gags_size = 10;
 
   posts_count = 0;
   gags_loaded = [];
@@ -21,38 +22,66 @@ export class AppComponent {
   	      .subscribe(
   	      	data => {
               this.posts_count = data['num'];
-              this.loadInitialGags(this.posts_count);
+              this.loadMoreGagsFrom(this.posts_count);
    	        },
    	        err => {
    	        	console.log('Couldnt retrieve current post count!')
    	        }
       );
+
+  	  this.createOnBottomReachLoadGags();
   }
 
-  private loadInitialGags(current_count) : void {
-  	var gags_idxs = Array.from(Array(current_count).keys());
-  	gags_idxs = gags_idxs.reverse();
-  	this.gags_loaded = gags_idxs.slice(0, this.initial_gag_count);
-  	for (var idx of this.gags_loaded) {
-  		console.log(idx);
-  		var post_data = {n: idx};
-  		this.posts_data.push(post_data);
-  		// Launch async load
-  		this.getPostData(post_data);
-  	}
+  private createOnBottomReachLoadGags() {
+  	 window.onscroll = () => {
+         let status = "not reached";
+         let windowHeight = "innerHeight" in window ? window.innerHeight
+             : document.documentElement.offsetHeight;
+         let body = document.body, html = document.documentElement;
+         let docHeight = Math.max(body.scrollHeight,
+             body.offsetHeight, html.clientHeight,
+             html.scrollHeight, html.offsetHeight);
+         let windowBottom = windowHeight + window.pageYOffset;
+         if (windowBottom >= docHeight) {
+           this.loadMoreGags();
+         }
+      };
   }
 
-  private getPostData(post_data):void {
+  private getPostData(post_data) : void {
 	  this.http.get('https://xkcd.com/' + post_data.n + '/info.0.json')
   	           .subscribe(
 		  	      	data => {
 		  	          post_data.data = data;
-		  	          console.log(data);
 		   	        },
 		   	        err => {
 		   	        	console.log('Couldnt retrieve gag data! (' + post_data.n + ')')
 		   	        }
       		   );	
-    	
+  }
+
+  private getLastGagIdx() : number {
+  	if (this.gags_loaded.length > 0)
+  		return this.gags_loaded[this.gags_loaded.length-1]
+  	else
+  		return this.posts_count;
+  }
+
+  private loadMoreGags() : void {
+  	this.loadMoreGagsFrom(this.getLastGagIdx());
+  }
+
+  private loadMoreGagsFrom(last_gag_idx) : void {
+  	var gags_idxs = Array.from(Array(last_gag_idx+1).keys());
+  	gags_idxs = gags_idxs.reverse();
+  	if (this.load_more_gags_size > 0)
+  		gags_idxs = gags_idxs.slice(0, this.load_more_gags_size);
+  	for (var idx of gags_idxs) {
+  		var post_data = {n: idx};
+  		this.posts_data.push(post_data);
+  		this.gags_loaded.push(idx);
+  		// Launch async load
+  		this.getPostData(post_data);
+  	}
   }
 }
