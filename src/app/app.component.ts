6254d14bc43d1bac7800bb3231ccb3dfebcd84a7
Javier Cabero <javier.cabero@hotmail.com>
2017-12-06 19:53:55 +0100

    Reads and shows last xkcd post images.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  xkcd_post_json;
+  title = 'app';	
+  post_nums = [{n:1925}, 
+  			   {n:1924}, 
+  			   {n:1923}, 
+  			   {n:1922}, 
+  			   {n:1921}, 
+  			   {n:1920}, 
+  			   {n:1919}
+  			   ];
+
+  constructor(private http: HttpClient) {
+  	  console.log('hEYYYELY')
+  	  http.get('https://xkcd.com/614/info.0.json')
+  	      .subscribe(
+  	      	data => {
+  	          console.log(data)
+              this.xkcd_post_json = data;
+   	        },
+   	        err => {
+   	        	console.log('Something went bad!')
+   	        }
+      );
+  	  for (var post_num of this.post_nums) {
+  	  	this.getImageURL(post_num);
+  	  }
+  }
+
+  public getImageURL(post_num):void {
+	  this.http.get('https://xkcd.com/' + post_num.n + '/info.0.json')
+  	           .subscribe(
+		  	      	data => {
+		  	          post_num.data = data;
+		   	        },
+		   	        err => {
+		   	        	console.log('Something went bad! (' + post_num.n + ')')
+		   	        }
+      		   );	
+    	
+  }
 }
