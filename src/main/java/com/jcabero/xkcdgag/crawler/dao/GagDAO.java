58308cea58613248a30727a1d3c22b711def7cbd
Javier Cabero <javier.cabero@hotmail.com>
2018-02-15 13:21:22 +0100

    fixed package names

--- src/main/java/com/jcabero/xkcdgag/crawler/dao/GagDAO.java
+++ src/main/java/com/jcabero/xkcdgag/crawler/dao/GagDAO.java
@@ -1,8 +1,8 @@
-package com.jcabero.xkcdgag.model;
+package com.jcabero.xkcdgag.crawler.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.jcabero.xkcdgag.daos.Gag;
+import com.jcabero.xkcdgag.crawler.model.Gag;
 
 public interface GagDAO extends CrudRepository<Gag, Long>{
 
--- src/main/java/com/jcabero/xkcdgag/crawler/model/Gag.java
+++ src/main/java/com/jcabero/xkcdgag/crawler/model/Gag.java
@@ -1,4 +1,4 @@
-package com.jcabero.xkcdgag.daos;
+package com.jcabero.xkcdgag.crawler.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
