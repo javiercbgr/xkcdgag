615a2c7c89354aedce81c148619ae829ba64ea2a
Javier Cabero <javier.cabero@hotmail.com>
2018-02-15 10:28:38 +0100

    Initial commit.

---
+++ src/main/java/com/jcabero/xkcdgag/crawler/xkcdgagcrawler/XkcdgagCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.jcabero.xkcdgag.crawler.xkcdgagcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XkcdgagCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XkcdgagCrawlerApplication.class, args);
+	}
+}
