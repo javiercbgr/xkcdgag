10e3ea8c20e3b2dbd4faf5eca085530fecc1e6b5
Javier Cabero <javier.cabero@hotmail.com>
2018-03-01 23:39:20 +0100

    eureka and config server seeing each other

--- xkcdgag-config/src/main/java/com/jcabero/xkcdgag/config/XkcdgagConfigApplication.java
+++ xkcdgag-config/src/main/java/com/jcabero/xkcdgag/config/XkcdgagConfigApplication.java
@@ -1,4 +1,4 @@
-package com.jcabero.xkcdgag.config.xkcdgagconfig;
+package com.jcabero.xkcdgag.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
