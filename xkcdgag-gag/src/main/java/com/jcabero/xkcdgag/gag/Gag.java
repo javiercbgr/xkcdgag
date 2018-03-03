43423536f499fe99401911873d7e7605db33657c
Javier Cabero <javier.cabero@hotmail.com>
2018-03-03 12:13:50 +0100

    exposed Gag REST CRUD API

--- xkcdgag-gag/src/main/java/com/jcabero/xkcdgag/gag/Gag.java
+++ xkcdgag-gag/src/main/java/com/jcabero/xkcdgag/gag/Gag.java
@@ -1,95 +1,95 @@
-//package com.jcabero.xkcdgag.gag;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//@Entity
-//public class Gag {
-//
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.AUTO)
-//	@Column(name="ID_GAG")
-//	private Long id;
-//	
-//	@Column(name="NUM_GAG")
-//	private Long numGag;
-//	
-//	@Column(name="IMG_URL")
-//	private String imgUrl;
-//	
-//	@Column(name="TITLE")
-//	private String title;
-//	
-//	@Column(name="LIKES")
-//	private Long likes;
-//	
-//	@Column(name="DISLIKES")
-//	private Long dislikes;
-//
-//	public Gag() {}
-//	
-//	public Gag(Long numGag, String imgUrl, String title, Long likes, Long dislikes) {
-//		this.numGag = numGag;
-//		this.imgUrl = imgUrl;
-//		this.title = title;
-//		this.likes = likes;
-//		this.dislikes = dislikes;
-//	}
-//
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	public Long getNumGag() {
-//		return numGag;
-//	}
-//
-//	public void setNumGag(Long numGag) {
-//		this.numGag = numGag;
-//	}
-//
-//	public String getImgUrl() {
-//		return imgUrl;
-//	}
-//
-//	public void setImgUrl(String imgUrl) {
-//		this.imgUrl = imgUrl;
-//	}
-//
-//	public String getTitle() {
-//		return title;
-//	}
-//
-//	public void setTitle(String title) {
-//		this.title = title;
-//	}
-//
-//	public Long getLikes() {
-//		return likes;
-//	}
-//
-//	public void setLikes(Long likes) {
-//		this.likes = likes;
-//	}
-//
-//	public Long getDislikes() {
-//		return dislikes;
-//	}
-//
-//	public void setDislikes(Long dislikes) {
-//		this.dislikes = dislikes;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Gag [id=" + id + ", numGag=" + numGag + ", imgUrl=" + imgUrl + ", title=" + title + ", likes=" + likes
-//				+ ", dislikes=" + dislikes + "]";
-//	}
-//}
+package com.jcabero.xkcdgag.gag;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Gag {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="ID_GAG")
+	private Long id;
+	
+	@Column(name="NUM_GAG")
+	private Long number;
+	
+	@Column(name="IMG_URL")
+	private String imgUrl;
+	
+	@Column(name="TITLE")
+	private String title;
+	
+	@Column(name="LIKES")
+	private Long likes;
+	
+	@Column(name="DISLIKES")
+	private Long dislikes;
+
+	public Gag() {}
+	
+	public Gag(Long number, String imgUrl, String title, Long likes, Long dislikes) {
+		this.number = number;
+		this.imgUrl = imgUrl;
+		this.title = title;
+		this.likes = likes;
+		this.dislikes = dislikes;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getNumber() {
+		return number;
+	}
+
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Long getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Long likes) {
+		this.likes = likes;
+	}
+
+	public Long getDislikes() {
+		return dislikes;
+	}
+
+	public void setDislikes(Long dislikes) {
+		this.dislikes = dislikes;
+	}
+
+	@Override
+	public String toString() {
+		return "Gag [id=" + id + ", numGag=" + number + ", imgUrl=" + imgUrl + ", title=" + title + ", likes=" + likes
+				+ ", dislikes=" + dislikes + "]";
+	}
+}
--- xkcdgag-gag/src/main/java/com/jcabero/xkcdgag/gag/GagDAO.java
+++ xkcdgag-gag/src/main/java/com/jcabero/xkcdgag/gag/GagDAO.java
@@ -1,13 +1,11 @@
-//package com.jcabero.xkcdgag.gag;
-//
-//import org.springframework.cloud.netflix.feign.FeignClient;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.data.repository.query.Param;
-//
-//@FeignClient("GAG")
-//public interface GagDAO extends CrudRepository<Gag, Long>{
-//
-//	  @Query("from Gag g where g.numGag=:numGag")
-//	  public Gag find(@Param("numGag") Long numGag);
-//}
+package com.jcabero.xkcdgag.gag;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "gag", path = "gag")
+public interface GagDAO extends PagingAndSortingRepository<Gag, Long> {
+
+	Gag number(@Param("number") Long number); 
+}
